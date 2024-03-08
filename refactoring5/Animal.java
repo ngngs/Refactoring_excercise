93b14b3186368223f088ef018776710338087921
Gyuseong Moon <47618270+ngngs@users.noreply.github.com>
2024-03-08 15:03:09 +0900

    Create Animal.java

---
+++ refactoring5/Animal.java
@@ -0,0 +1,13 @@
+package refactoring5;
+
+public class Animal {
+    String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
